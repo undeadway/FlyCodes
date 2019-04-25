e6c4ce748024b5680ba93ae35d35d54538a6e144
undeadway <hzwaygc@gmail.com>
2019-04-26 01:39:22 +0800

    恢复 plugin.js

---
+++ src/lib/HitOn/dist/plugin.js
@@ -0,0 +1,47 @@
+
+let highLighter = FlyHighLighter.execute;
+
+var CodeRegExp = {
+	regexp: /```(.*[\r\n]+)((.|\s)*?)```/,
+	code: "$2",
+	name: "$1"
+};
+
+function getCodeParser() {
+
+	let toCode = [];
+
+	return {
+		before: function (str) {
+
+			while (CodeRegExp.regexp.test(str)) {
+
+				var codeWithLang;
+
+				var code = RegExp[CodeRegExp.code];
+				var name = RegExp[CodeRegExp.name];
+
+				if (name) {
+					codeWithLang = "```" + name + code + "```";
+				} else {
+					codeWithLang = "```" + code + "```";
+				}
+
+				str = str.replace(codeWithLang, "[toCode" + toCode.length + "]");
+				code = highLighter(code, String.trim(name));
+
+				toCode.push(code);
+			}
+
+			return str;
+		},
+		after: function (str) {
+			Object.forEach(toCode, function (i, obj) {
+				str = str.replace("[toCode" + i + "]", obj);
+			});
+			return str;
+		}
+	}
+}
+
+module.exports = () => [getCodeParser()];
